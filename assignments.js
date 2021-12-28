5bf104cf332f1656b053f4793bf63fde3ea56149
XhesB <xhesikabanushi28@gmail.com>
2021-12-28 14:52:30 +0100

    initial commit

---
+++ assignments.js
@@ -0,0 +1,8 @@
+/*
+let country = 'Albania';
+let continent = 'Europe';
+let population = 3000000;
+console.log(country);
+console.log(continent);
+console.log(population);
+*/
---
+++ script.js
@@ -0,0 +1,131 @@
+/*
+      let js = "amazing";
+    //   console.log(40 + 8 + 23 - 10);
+      console.log('Jonas');
+      console.log(23);
+      let firstName = "Jessica";
+      console.log(firstName);
+      let PI = 3.1415;
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+console.log(myFirstJob);
+
+
+let javascriptIsFun = true; 
+console.log(javascriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof javascriptIsFun);
+//console.log(typeof 23);
+//console.log(typeof 'Jessica');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+
+var job = "programmer";
+job = 'teacher';
+
+// Math operators
+const now = 2022;
+const ageJessica = now - 1994;
+const ageMirel = now - 1993;
+console.log(ageJessica, ageMirel);
+console.log(ageJessica * 2, ageJessica / 10, 2 ** 3);
+const firstName = 'Jessica';
+const lastName = 'Banushi';
+console.log(firstName + " " + lastName);
+
+// Assignment operators
+let x = 10 + 5; //15
+x += 10; // x = x +10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x= x + 1 = 101
+x--; // x = x - 1 = 100
+x--;
+console.log(x);
+
+// Comparison operators 
+console.log(ageJessica > ageMirel); // >, <, >=, <=
+console.log(ageJessica >= 18);
+const isFullAge = ageJessica >= 18; 
+console.log(now - 1994 > now - 1993);
+
+const now = 2022;
+const ageJessica = now - 1994;
+const ageMirel = now - 1993;
+
+console.log(now - 1994 > now - 1993);
+
+let x, y; 
+x = y = 25-10-5;
+console.log(x, y);
+
+const averageAge = (ageMirel + ageJessica) / 2;
+console.log(ageJessica, ageMirel, averageAge);
+*/
+
+/* Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+m tall.
+GOOD LUCK 😀 */
+// Data 1
+/*
+let weight; 
+let height;
+const BMI = weight / height ** 2;
+const Markweight = 78;
+const Markheight = 1.69;
+const MarkBMI = Markweight / Markheight ** 2;
+const Johnweight = 92; 
+const Johnheight = 1.95; 
+console.log(MarkBMI); 
+const JohnBMI = Johnweight / Johnheight ** 2;
+console.log(JohnBMI);
+
+// Data 2
+let weight; 
+let height;
+const BMI = weight / height ** 2;
+const Markweight = 95;
+const Markheight = 1.88;
+const MarkBMI = Markweight / Markheight ** 2;
+const Johnweight = 85; 
+const Johnheight = 1.76; 
+console.log(MarkBMI); 
+const JohnBMI = Johnweight / Johnheight ** 2;
+console.log(JohnBMI);
+const markHigherBMI = MarkBMI > JohnBMI;
+console.log(MarkBMI > JohnBMI);
+*/
+
+const firstName = 'Jessica'; 
+const job = 'researcher';
+const birthYear = 1994;
+
+
