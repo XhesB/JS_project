3adb915e0db2deaeaf50af4da3ed1eb08be830aa
Xhesb <xhesikabanushi28@gmail.com>
2021-12-31 11:32:05 +0100

    challenge 3

--- script.js
+++ script.js
@@ -257,5 +257,112 @@ if(height) {
   console.log('Height is UNDEFINED');
 }
 */
-const age = 18; 
-if(age === 18) console.log('You just became an adult :D');
+/*
+const age = '18'; 
+if(age === 18) console.log('You just became an adult :D (strict)');
+
+if(age == 18) console.log('You just became an adult :D (loose)');
+
+const favourite = Number (prompt("What's your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) { // '23' == 23 | 23 === 23
+  console.log('Cool! 23 is an amazing number!');
+} else if(favourite === 7) {
+  console.log('7 is also a cool number');
+} else if (favourite === 9) {
+  console.log('9 is also cool');
+}
+else {
+  console.log('Number is not 23 or 7 or 9');
+}
+
+if(favourite !== 23) console.log('Why not 23?');
+*/
+/*
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+const shouldDrive = hasDriversLicense && hasGoodVision; 
+
+// if(shouldDrive) {
+//   console.log('Sarah is able to drive!');
+// } else {
+//   console.log('Someone else should drive...');
+// }
+
+const isTired = false; // C 
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if(hasDriversLicense && hasGoodVision && !isTired) {
+  console.log('Sarah is able to drive!');
+} else {
+  console.log('Someone else should drive...');
+}
+*/
+
+// Coding Challenge #3
+
+// There are two gymnastics teams, Dolphins and Koalas. They compete against each
+// other 3 times. The winner with the highest average score wins a trophy!
+// Your tasks:
+// 1. Calculate the average score for each team, using the test data below
+// 2. Compare the team's average scores to determine the winner of the competition,
+// and print it to the console. Don't forget that there can be a draw, so test for that
+// as well (draw means they have the same average score)
+// 3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
+// team only wins if it has a higher score than the other team, and the same time a
+// score of at least 100 points. Hint: Use a logical operator to test for minimum
+// score, as well as multiple else-if blocks 😉
+// 4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
+// both teams have the same score and both have a score greater or equal 100
+// points. Otherwise, no team wins the trophy
+// Test data:
+// § Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+// § Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+// § Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+// GOOD LUCK 😀
+
+  // const scoreDolphins = (96 + 108 + 89) / 3;
+  // const scoreKoalas = (88 + 91 + 110) / 3;
+  // console.log(scoreDolphins, scoreKoalas);
+  // const hasHigherPoints = scoreDolphins > scoreKoalas; 
+  // if(scoreDolphins > scoreKoalas) {
+  //   console.log('Dolphins win the trophy :D');
+  // } else if(scoreDolphins < scoreKoalas) {
+  //   console.log('Koalas win the trophy');
+  // } else if (scoreDolphins === scoreKoalas) {
+  //   console.log('Both teams win!');
+  // }
+
+// Bonus 1 
+  // const scoreDolphins = (97 + 112 + 101) / 3;
+  // const scoreKoalas = (109 + 95 + 123) / 3;
+  // console.log(scoreDolphins, scoreKoalas);
+  // const hasHigherPoints = scoreDolphins > scoreKoalas; 
+  // if(scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+  //   console.log('Dolphins win the trophy :D');
+  // } else if(scoreDolphins < scoreKoalas && scoreKoalas >= 100) {
+  //   console.log('Koalas win the trophy');
+  // } 
+
+  // Bonus 2
+
+  const scoreDolphins = (97 + 112 + 101) / 3;
+  const scoreKoalas = (109 + 95 + 106) / 3;
+  console.log(scoreDolphins, scoreKoalas);
+  const hasHigherPoints = scoreDolphins > scoreKoalas; 
+  if(scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+    console.log('Dolphins win the trophy :D');
+  } else if(scoreDolphins < scoreKoalas && scoreKoalas >= 100) {
+    console.log('Koalas win the trophy');
+  } else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {
+    console.log('Both teams win!');
+  } else { 
+    console.log('No one wins');
+  }
